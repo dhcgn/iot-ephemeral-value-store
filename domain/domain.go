aee017bd809db8e5426ea6c04bc953c9c8fe9fc5
Daniel <dhcgn@users.noreply.github.com>
2024-06-15 17:31:53 +0200

    timestamp handling (#13)

--- domain/domain.go
+++ domain/domain.go
@@ -4,7 +4,9 @@ import (
 	"crypto/rand"
 	"crypto/sha256"
 	"encoding/hex"
+	"errors"
 	"fmt"
+	"strings"
 )
 
 func GenerateRandomKey() string {
@@ -23,4 +25,16 @@ func DeriveDownloadKey(uploadKey string) (string, error) {
 
 	hash := sha256.Sum256([]byte(uploadKey))
 	return hex.EncodeToString(hash[:]), nil
+}
+
+func ValidateUploadKey(uploadKey string) error {
+	uploadKey = strings.ToLower(uploadKey)
+	decoded, err := hex.DecodeString(uploadKey)
+	if err != nil {
+		return errors.New("uploadKey must be a 256 bit hex string")
+	}
+	if len(decoded) != 32 {
+		return errors.New("uploadKey must be a 256 bit hex string")
+	}
+	return nil
 }
---
+++ httphandler/datahandling.go
@@ -0,0 +1,79 @@
+package httphandler
+
+import (
+	"strings"
+	"time"
+)
+
+func collectParams(params map[string][]string) map[string]string {
+	paramMap := make(map[string]string)
+	for key, values := range params {
+		if len(values) > 0 {
+			sanitizedValue := sanitizeInput(values[0])
+			paramMap[key] = sanitizedValue
+		}
+	}
+	return paramMap
+}
+
+func addTimestampToThisData(paramMap map[string]string, path string) {
+	// if using patch and path is empty than add a timestamp with the value suffix
+	if path == "" {
+		allKeys := make([]string, 0, len(paramMap))
+		for k := range paramMap {
+			allKeys = append(allKeys, k)
+		}
+		// add a timestamp with the value suffix for all keys
+		timestamp := time.Now().UTC().Format(time.RFC3339)
+		for _, k := range allKeys {
+			paramMap[k+"_timestamp"] = timestamp
+		}
+	}
+
+	timestamp := time.Now().UTC().Format(time.RFC3339)
+	paramMap["timestamp"] = timestamp
+}
+
+func (c Config) modifyData(downloadKey string, paramMap map[string]string, path string, isPatch bool) (map[string]interface{}, error) {
+	var dataToStore map[string]interface{}
+
+	if isPatch {
+		existingData, err := c.StorageInstance.Retrieve(downloadKey)
+		if err != nil {
+			return nil, err
+		}
+		mergeData(existingData, paramMap, strings.Split(path, "/"))
+		dataToStore = existingData
+	} else {
+		dataToStore = make(map[string]interface{})
+		for k, v := range paramMap {
+			dataToStore[k] = v
+		}
+	}
+
+	// add timestamp so that root level timestamp is always the latest of any updated value
+	timestamp := time.Now().UTC().Format(time.RFC3339)
+	dataToStore["timestamp"] = timestamp
+
+	return dataToStore, nil
+}
+
+func mergeData(existingData map[string]interface{}, newData map[string]string, path []string) {
+	if len(path) == 0 || (len(path) == 1 && path[0] == "") {
+		for k, v := range newData {
+			existingData[k] = v
+		}
+		return
+	}
+
+	currentKey := path[0]
+	if _, exists := existingData[currentKey]; !exists {
+		existingData[currentKey] = make(map[string]interface{})
+	}
+
+	if nestedMap, ok := existingData[currentKey].(map[string]interface{}); ok {
+		mergeData(nestedMap, newData, path[1:])
+	} else {
+		existingData[currentKey] = newData
+	}
+}
