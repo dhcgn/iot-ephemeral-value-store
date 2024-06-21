93a72a0e048981580e1b45e62181c1f43d8355ca
Daniel <dhcgn@users.noreply.github.com>
2024-06-21 21:11:58 +0200

    add tests

--- httphandler/config.go
+++ httphandler/config.go
@@ -1,10 +1,6 @@
 package httphandler
 
 import (
-	"encoding/json"
-	"html"
-	"net/http"
-
 	"github.com/dhcgn/iot-ephemeral-value-store/stats"
 	"github.com/dhcgn/iot-ephemeral-value-store/storage"
 )
@@ -12,14 +8,4 @@ import (
 type Config struct {
 	StorageInstance storage.Storage
 	StatsInstance   *stats.Stats
-}
-
-func sanitizeInput(input string) string {
-	// Escapes HTML special characters like <, >, & and quotes
-	return html.EscapeString(input)
-}
-
-func jsonResponse(w http.ResponseWriter, data interface{}) {
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(data)
 }
